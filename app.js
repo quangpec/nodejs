a8b6f169783de2d6ba57dfccef347d058591fe73
quanglnFX13841 <quanglnq0305@gmail.com>
2022-03-24 17:57:59 +0700

    Lab1.4

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const http = require('http');
 const server = http.createServer((req,res)=>{
-    console.log(req.url, req.method, req.headers);
+
+    //console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    if (url === '/'){
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<head><title>My First Page</title></head>');
+        res.write('<body><form action="/message" method ="POST"><input type ="text" name = "message"></input><button type ="submit">send</button></form></body>');
+        res.write('</html>');
+        return res.end;
+    }
     res.setHeader('Content-Type','text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello from my Node.js server!</h1></body>')
+    res.write('<body><h1>Hello from my Node.js server!</h1></body>');
     res.write('</html>');
-    res.end;
-
+    return res.end;
 });
 server.listen(3000);
