5b2a2128ac0f864aaf087ff12ec0dd20aa18437c
quanglnFX13841 <quanglnq0305@gmail.com>
2022-03-25 22:21:20 +0700

    lab1.5

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
 const http = require('http');
-const server = http.createServer((req,res)=>{
+const fs = require('fs');
 
-    //console.log(req.url, req.method, req.headers);
-    const url = req.url;
-    if (url === '/'){
-        res.setHeader('Content-Type','text/html');
-        res.write('<html>');
-        res.write('<head><title>My First Page</title></head>');
-        res.write('<body><form action="/message" method ="POST"><input type ="text" name = "message"></input><button type ="submit">send</button></form></body>');
-        res.write('</html>');
-        return res.end;
-    }
-    res.setHeader('Content-Type','text/html');
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === '/') {
     res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello from my Node.js server!</h1></body>');
+    res.write('<head><title>Enter Message</title><head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
     res.write('</html>');
-    return res.end;
+    return res.end();
+  }
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'DUMMY');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title><head>');
+  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+  res.write('</html>');
+  res.end();
 });
+
 server.listen(3000);
