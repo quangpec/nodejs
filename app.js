59a61bbb57dda61d718894b1cd1e47cf8e06e495
quanglnFX13841 <quanglnq0305@gmail.com>
2022-03-28 10:49:51 +0700

    lab2.2

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const http = require('http');
 const express = require('express');
 
 const app = express();
+app.use((req,res,next) =>{
+ console.log('in the middelware!');
+ next();// tiếp tục các middelware khác
+});
+app.use((req,res,next) =>{
+    console.log('in another middelware!');
+    //...
+   });
 const server = http.createServer(app);
 //console.log(routes.somtext);
 server.listen(3000);
