9738c9325e90658b1d18133b2059ca3987594d7a
quanglnFX13841 <quanglnq0305@gmail.com>
2022-03-28 16:22:22 +0700

    lab2.8

--- app.js
+++ app.js
@@ -9,8 +9,9 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(adminRoutes);
 app.use(shopRoutes);
-
-
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found</h1>');
+})
 const server = http.createServer(app);
 //console.log(routes.somtext);
 server.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/',(req,res,next) =>{
-    console.log('This always runs!');
-    next();// tiếp tục các middelware khác
-   });
-
-router.use('/',(req,res,next)=>{
+router.get('/',(req,res,next)=>{
     console.log('in another middelware!');
     res.send('<h1> Hello from next.js!</h1>');
+    next();
 });
 
 module.exports = router;
