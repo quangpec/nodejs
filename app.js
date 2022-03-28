cce6a9d54c82f26faf54c4dcc06322b352a4f345
quanglnFX13841 <quanglnq0305@gmail.com>
2022-03-28 15:05:28 +0700

    lab2.3

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 const http = require('http');
+const bodyParser = require('body-parser');
 //const routes = require('./routes');
 
 const express = require('express');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/',(req,res,next) =>{
  console.log('This always runs!');
  next();// tiếp tục các middelware khác
 });
 app.use('/add-product',(req,res,next) =>{
-    res.send('<h1> The "Add Product" page</h1>');
+    res.send('<form action = "/product" method ="POST"><input type ="text" name ="title"></input> <br> <input type ="text" name ="id"></input> <button type ="submit" >Add Product</button></form>');
     console.log('in another middelware!');
    
    });
+app.use('/product',(req,res,next)=>{
+ console.log(req.body);
+ res.redirect('/');
+})
 app.use('/',(req,res,next)=>{
     
     console.log('in another middelware!');
