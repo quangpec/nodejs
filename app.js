f1e7ad665106b4645cb98298ef85718f08282a45
quanglnFX13841 <quanglnq0305@gmail.com>
2022-03-28 11:39:27 +0700

    lab2.4

--- app.js
+++ app.js
@@ -4,14 +4,21 @@ const http = require('http');
 const express = require('express');
 
 const app = express();
-app.use((req,res,next) =>{
- console.log('in the middelware!');
+app.use('/',(req,res,next) =>{
+ console.log('This always runs!');
  next();// tiếp tục các middelware khác
 });
-app.use((req,res,next) =>{
+app.use('/add-product',(req,res,next) =>{
+    res.send('<h1> The "Add Product" page</h1>');
+    console.log('in another middelware!');
+   
+   });
+app.use('/',(req,res,next)=>{
+    
     console.log('in another middelware!');
     res.send('<h1> Hello from next.js!</h1>');
-   });
+
+})
 const server = http.createServer(app);
 //console.log(routes.somtext);
 server.listen(3000);
