e0357a236d2b3bf16f97de073d96e474f79d81cd
quanglnFX13841 <quanglnq0305@gmail.com>
2022-03-28 10:57:33 +0700

    lab2.3

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use((req,res,next) =>{
 });
 app.use((req,res,next) =>{
     console.log('in another middelware!');
-    //...
+    res.send('<h1> Hello from next.js!</h1>');
    });
 const server = http.createServer(app);
 //console.log(routes.somtext);
