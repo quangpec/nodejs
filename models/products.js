54890bad404f84e72027f97d0ff253ed4fa532c4
quanglnFX13841 <quanglnq0305@gmail.com>
2022-03-31 15:12:01 +0700

    lab4.6

--- models/products.js
+++ models/products.js
@@ -6,29 +6,30 @@ const p = path.join(
   'data',
   'products.json'
 );
+const getProductFromfile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if(err) {
+      cb([]);
+    } else{
+    cb(JSON.parse(fileContent));
+    }
+  });
+}
 
 module.exports = class Product {
   constructor(t) {
     this.title = t;
   }
   save() {
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    getProductFromfile(products=>{
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
       });
     });
-  }
+    fs.readFile(p,(err,fileContent)=>{})
+  };
   static fetchAll(cb) {
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        cb([]);
-      }
-      cb(JSON.parse(fileContent));
-    });
+    getProductFromfile(cb);
   }
 };
