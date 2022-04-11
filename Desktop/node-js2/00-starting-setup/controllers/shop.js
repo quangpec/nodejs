ab68501d8faa514b4388413af4683119d9ff83fd
quanglnFX13841 <quanglnq0305@gmail.com>
2022-04-11 23:34:54 +0700

    lab5.22

--- Desktop/node-js2/00-starting-setup/controllers/shop.js
+++ Desktop/node-js2/00-starting-setup/controllers/shop.js
@@ -69,6 +69,7 @@ exports.getCart = (req, res, next) => {
     return cart.getProducts()
   })
   .then(products=>{
+    console.log(products);
     res.render('shop/cart', {
             path: '/cart',
             pageTitle: 'Your Cart',
@@ -96,13 +97,40 @@ exports.getCart = (req, res, next) => {
   // })
 
 };
+
 exports.postCart = (req, res, next) => {
-  const id = req.body.id;
-  Product.fetchProductId(id, (product) => {
-    Cart.addTocart(id, product.price);
-  })
-  res.redirect('/');
-}
+  const prodId  = req.body.id;
+  let fetchedCart;
+  let newQuantity = 1;
+  req.user
+    .getCart()
+    .then(cart => {
+      fetchedCart = cart;
+      return cart.getProducts({ where: { id: prodId } });
+    })
+    .then(products => {
+      let product;
+      if (products.length > 0) {
+        product = products[0];
+      }
+
+      if (product) {
+        const oldQuantity = product.cartItem.quantity;
+        newQuantity = oldQuantity + 1;
+        return product;
+      }
+      return Product.findByPk(prodId);
+    })
+    .then(product => {
+      return fetchedCart.addProduct(product, {
+        through: { quantity: newQuantity }
+      });
+    })
+    .then(() => {
+      res.redirect('/cart');
+    })
+    .catch(err => console.log(err));
+};
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
