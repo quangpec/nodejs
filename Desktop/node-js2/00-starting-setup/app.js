0c68a2086f1b79912e4801f8c6623ddd93dfe546
quanglnFX13841 <quanglnq0305@gmail.com>
2022-04-11 10:14:55 +0700

    lab5.10

--- Desktop/node-js2/00-starting-setup/app.js
+++ Desktop/node-js2/00-starting-setup/app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
-// const db = require('./util/database');
+ const sequelize = require('./util/database');
 
 const app = express();
 
@@ -28,5 +28,14 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+sequelize.sync().then( result =>{
+    console.log(result);
+}
+
+).catch(err=>{
+    console.log(err);
+}
+
+)
 
 app.listen(3000);
