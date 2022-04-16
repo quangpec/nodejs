2c90302b646a9cb0bf4388d82200c7b29e5b5efa
quanglnFX13841 <quanglnq0305@gmail.com>
2022-04-16 09:42:14 +0700

    lab7.4

--- Desktop/node-js2/00-starting-setup/controllers/admin.js
+++ Desktop/node-js2/00-starting-setup/controllers/admin.js
@@ -29,7 +29,7 @@ exports.postAddProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then(products => {
       res.render('admin/products', {
         prods: products,
