8f71ca7ab0db7ee28b91d54aaee3a58b842c42f6
quanglnFX13841 <quanglnq0305@gmail.com>
2022-04-27 15:15:57 +0700

    Lab08-2.1

--- Desktop/node-js2/00-starting-setup/app.js
+++ Desktop/node-js2/00-starting-setup/app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
+  User.findById('625c31156c79456b20d3ede4')
     .then(user => {
       req.user = user;
       next();
