49745f9c7b35ebc759888463dc910837aac9a184
quanglnFX13841 <quanglnq0305@gmail.com>
2022-04-29 23:12:34 +0700

    Lab08-15

--- Desktop/node-js2/00-starting-setup/controllers/auth.js
+++ Desktop/node-js2/00-starting-setup/controllers/auth.js
@@ -18,41 +18,59 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('625c31156c79456b20d3ede4')
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email: email })
     .then(user => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save(err => {
-        console.log(err);
-        res.redirect('/');
-      });
+      console.log(user);
+      if (!user) {
+        return res.redirect('/login');
+      }
+      return bcrypt
+        .compare(password, user.password)
+        .then(doMacth => {
+          if (doMacth) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session
+              .save(err => {
+                console.log(err);
+                res.redirect('/');
+              });
+          }
+          return res.redirect('/login')
+        })
+        .catch(err => {
+          console.log(err)
+          return res.redirect('/login');
+        })
     })
-    .catch(err => console.log(err));
-};
+    .catch(err => console.log(err)) 
+}
 
 exports.postSignup = (req, res, next) => {
   const email = req.body.email;
-  const password= req.body.password;
+  const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
-  User.findOne({email: email}).then(user =>{
-    if(user){
+  User.findOne({ email: email }).then(user => {
+    if (user) {
       return res.redirect('/login');
     }
-    return bcrypt.hash(password,12)
-    .then(hashPassword=>{
-      user = new User({
-        email : email,
-        password: hashPassword,
-        cart : {
-          items:[]
-        }
+    return bcrypt.hash(password, 12)
+      .then(hashPassword => {
+        user = new User({
+          email: email,
+          password: hashPassword,
+          cart: {
+            items: []
+          }
+        })
+        return user.save();
       })
-      return user.save();
-    })
-  }).then(result =>{
+  }).then(result => {
     return res.redirect('/login');
-  }) 
-  .catch(err => console.log(err));
+  })
+    .catch(err => console.log(err));
 };
 
 
