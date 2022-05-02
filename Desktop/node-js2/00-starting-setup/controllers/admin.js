f289a00aff2e8b8d527d0a448568fad510f1d471
quanglnFX13841 <quanglnq0305@gmail.com>
2022-05-02 22:48:34 +0700

    Lab09_11

--- Desktop/node-js2/00-starting-setup/controllers/admin.js
+++ Desktop/node-js2/00-starting-setup/controllers/admin.js
@@ -1,10 +1,13 @@
 const Product = require('../models/product');
+const { validationResult } = require('express-validator');
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    editing: false
+    editing: false,
+    hasError: false,
+    errorMessage: null,
   });
 };
 
@@ -13,6 +16,24 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
+  const errors = validationResult(req);
+  console.log(errors);
+  if (!errors.isEmpty()) {
+    return res.status(422).render('admin/edit-product', {
+      pageTitle: 'Add Product',
+      path: '/admin/add-product',
+      errorMessage: errors.array()[0].msg,
+      editing: false,
+      hasError: true,
+      product: {
+        title: title,
+        imageUrl: imageUrl,
+        price: price,
+        description: description
+      }
+    });
+
+  }
   const product = new Product({
     title: title,
     price: price,
@@ -47,6 +68,8 @@ exports.getEditProduct = (req, res, next) => {
         pageTitle: 'Edit Product',
         path: '/admin/edit-product',
         editing: editMode,
+        hasError: false,
+        errorMessage: null,
         product: product
       });
     })
